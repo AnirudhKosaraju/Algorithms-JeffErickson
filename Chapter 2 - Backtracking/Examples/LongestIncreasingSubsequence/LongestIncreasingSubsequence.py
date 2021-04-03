e9550416fe9ec04b84a9422899acc8620b0b4da2
Anirudh Kosaraju <anirudh.kosaraju@gmail.com>
2021-04-03 09:21:03 -0500

    LongestIncreasingSubsequence

---
+++ Chapter 2 - Backtracking/Examples/LongestIncreasingSubsequence/LongestIncreasingSubsequence.py
@@ -0,0 +1,24 @@
+
+'''
+Suppose we are given a sequence of integers and we need to find the
+longest subsequence whose elements are in increasing order.
+'''
+
+from typing import List
+
+def LongestIncreasingSubsequence(X: List[int]) -> int:
+    return solve(X, 0, 0, len(X))
+
+def solve(X, prev, i, n) -> int:
+    if i == n:
+        return 0
+    elif X[i] <= prev:
+        return solve(X, prev, i+1, n)
+    else:
+        skip = solve(X, prev, i+1, n)
+        take = solve(X, X[i], i+1, n) + 1
+        return max(skip, take)
+
+
+X = [1,3,2,5,4,6,5,7,6,8,7,9,8,10,9]
+print(LongestIncreasingSubsequence(X))
---
+++ Chapter 2 - Backtracking/Examples/LongestIncreasingSubsequence/LongestIncreasingSubsequenceTest.py
@@ -0,0 +1,24 @@
+
+from LongestIncreasingSubsequence import LongestIncreasingSubsequence
+import unittest
+
+class TestCountSubsetSum(unittest.TestCase):
+
+    def test_01(self):
+        X = [1,2,3,4,5,6,7,8,9,10]
+        self.assertEqual(LongestIncreasingSubsequence(X), 10)
+
+    def test_02(self):
+        X = [1,2,1,3,1,4,1,5]
+        self.assertEqual(LongestIncreasingSubsequence(X), 5)
+
+    def test_03(self):
+        X = [5,4,3,2,1]
+        self.assertEqual(LongestIncreasingSubsequence(X), 1)
+
+    def test_04(self):
+        X = [1,3,2,5,4,6,5,7,6,8,7,9,8,10,9]
+        self.assertEqual(LongestIncreasingSubsequence(X), 8)
+
+if __name__ == '__main__':
+    unittest.main()
